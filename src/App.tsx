e78a5b41a76d268c8c1510f4a79126f23cb516ed
devwqc <devwqc@gmail.com>
2025-08-05 13:03:25 +0900

    feat: react-query 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { getQueryClient } from './lib/react-query';
+
+const queryClient = getQueryClient();
+
 function App() {
-  return <div className="text-2xl text-amber-400">tailwindcss</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="text-2xl text-amber-400">tailwindcss</div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
